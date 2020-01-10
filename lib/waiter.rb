f25dd41234dc7fa837845d5a80b20a2676ac86a6
Stela Capsa <stela.capsa@gmail.com>
2020-01-10 19:29:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,10 +11,9 @@ def self.all
   @@all 
 end 
 
-def new_meal(customer, total, tip = 0)
-  Meal.new(customer, self, total, tip)
-end 
-
+def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
 def meals
   Meal.all.select do |meal|
     meal.waiter == self #checking for waiter now
