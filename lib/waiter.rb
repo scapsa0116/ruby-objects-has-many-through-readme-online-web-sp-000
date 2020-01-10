30a9dbe02860a46a6b6668818939d2a0f5b8613a
Stela Capsa <stela.capsa@gmail.com>
2020-01-10 19:34:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,8 +11,8 @@ def self.all
   @@all 
 end 
 
-def new_meal(customer,waiter, total, tip=0)
-    Customer.new(waiter, self, total, tip)
+def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end
   
   def meals
